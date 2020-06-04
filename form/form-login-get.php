50e2412dd0270941317dcfbf08845ac038e4d817
Khoa <nguyentudangkhoa@gmail.com>
2020-06-04 21:25:52 +0700

    update command

--- form/form-login-get.php
+++ form/form-login-get.php
@@ -1,4 +1,5 @@
 <?php
+//Post gửi dữ liệu lên server và hiện thông tin form lên link điều hướng
 if(isset($_GET['btnSub'])){
     $email = $_GET['email'];// email
     $pwd = $_GET['password'];// password
--- form/form-login-post.php
+++ form/form-login-post.php
@@ -4,15 +4,15 @@ require_once('../include/db.include.php');
 //include
 //include_once
 
-
+//Post gửi dữ liệu lên server nhưng không hiện thông tin form lên link điều hướng
 if(isset($_POST['btnSub'])){
     $email = $_POST['email'];// email
     $pwd = $_POST['password'];// password
-    $sql = "SELECT * FROM users WHERE username = '$email' AND password = '$pwd'";
-    $result = $pdo->query($sql);
+    $sql = "SELECT * FROM users WHERE username = '$email' AND password = '$pwd'";//sql querry command
+    $result = $pdo->query($sql);// querry sql
     if($email == "" && $pwd == ""){
         echo "email and password is empty";
-    }else if($result->rowCount()>0){
+    }else if($result->rowCount()>0){// check have user
         echo "Loged in";
     }
 
